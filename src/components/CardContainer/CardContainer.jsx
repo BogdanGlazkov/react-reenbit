09abe96a68011c60b220a46d149208e90ccfa629
BogdanGlazkov <bglazkov@i.ua>
2023-03-14 00:08:09 +0200

    Add Google auth

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { HiSearch } from 'react-icons/hi';
 import Card from 'components/Card/Card';
 import { getCharacters } from 'services/charactersService';
 import s from './CardContainer.module.css';
@@ -8,30 +9,59 @@ const CardContainer = () => {
   const [query, setQuery] = useState('');
   const [status, setStatus] = useState('idle');
 
+  const onInputChange = e => {
+    setQuery(e.target.value);
+    setCharacters(
+      characters.filter(el =>
+        el.name.toLowerCase().includes(query.toLowerCase())
+      )
+    );
+    console.log(query);
+  };
+
   useEffect(() => {
-    getCharacters().then(res => setCharacters(res));
-
-    if (query) {
-      setStatus('pending');
-
-      getCharacters()
-        .then(res => {
-          if (!res.length) {
-            setStatus('idle');
-            setCharacters([]);
-            return;
-          }
-          setStatus('resolved');
-          setCharacters(() => res);
-        })
-        .catch(() => setStatus('rejected'));
-    }
-  }, [query]);
+    setStatus('pending');
+    getCharacters()
+      .then(res => setCharacters(res))
+      .then(setStatus('resolved'))
+      .catch(() => setStatus('rejected'));
+    // if (query) {
+    //   setStatus('pending');
+    // .then(res => {
+    //   if (!res.length) {
+    //     setStatus('idle');
+    //     setCharacters([]);
+    //     return;
+    //   }
+    //   setStatus('resolved');
+    //   setCharacters(() => res);
+    // })
+    // .catch(() => setStatus('rejected'));
+    // }
+  }, []);
 
   return (
     <>
       {status === 'pending' && <h2>Downloading...</h2>}
       {status === 'rejected' && <h2>Something went wrong...</h2>}
+      <form className={s.form}>
+        <HiSearch
+          width="24"
+          height="24"
+          aria-label="search"
+          color="rgba(0, 0, 0, 0.54)"
+        />
+        <input
+          className={s.input}
+          type="text"
+          autoComplete="off"
+          onChange={onInputChange}
+          autoFocus
+          value={query}
+          placeholder="Filter by name..."
+        />
+      </form>
+
       <section className={s.container}>
         {!characters.length
           ? null
--- src/services/charactersService.js
+++ src/services/charactersService.js
@@ -2,7 +2,7 @@ export const getCharacters = async () => {
   try {
     const res = await fetch('https://rickandmortyapi.com/api/character');
     const data = await res.json();
-    return data.results;
+    return data.results.sort((a, b) => a.name.localeCompare(b.name));
   } catch (error) {
     throw new Error(error);
   }
