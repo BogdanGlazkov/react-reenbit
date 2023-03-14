cb9cbd4b01da32c2208aa805f25147906f0858d2
BogdanGlazkov <bglazkov@i.ua>
2023-03-14 15:57:49 +0200

    Fix deploy

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <BrowserRouter> */}
     <BrowserRouter basename="/reenbit-70f6a/">
       <App />
     </BrowserRouter>
