b36dffdb7fd957b29b00fd433ab04ba489db7943
BogdanGlazkov <bglazkov@i.ua>
2023-03-14 15:25:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/reenbit-70f6a/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
